b63f905d61ca4179b192d388398c9a4abc855677
Andrew Tang <andrewtang0926@gmail.com>
2023-12-12 21:24:57 -0500

    scripts

---
+++ scripts/gen_summ.sh
@@ -0,0 +1,9 @@
+python api_request_parallel_processor.py \
+  --requests_filepath articles.jsonl \
+  --save_filepath summaries.jsonl \
+  --request_url https://api.openai.com/v1/chat/completions \
+  --max_requests_per_minute 1000 \
+  --max_tokens_per_minute 10000 \
+  --token_encoding_name cl100k_base \
+  --max_attempts 5 \
+  --logging_level 20
---
+++ scripts/gpt-3.5-test.sh
@@ -0,0 +1,9 @@
+python api_request_parallel_processor.py \
+  --requests_filepath gpt-3.5-test-prompts.jsonl \
+  --save_filepath gpt-3.5-test-output.jsonl \
+  --request_url https://api.openai.com/v1/chat/completions \
+  --max_requests_per_minute 3500 \
+  --max_tokens_per_minute 160000 \
+  --token_encoding_name cl100k_base \
+  --max_attempts 5 \
+  --logging_level 20
